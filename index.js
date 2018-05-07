cfc6b1214bd9e62ef5896f9c37bd4dc7b17db5b1
Shanghq <shanghq@raiyee.com>
2018-05-07 15:28:00 +0800

    update

---
+++ index.js
@@ -0,0 +1,52 @@
+const child_process = require('child_process')
+const bookmarks = {
+    'bd': {name: '百度', url: 'https://www.baidu.com'},
+    'bl': {name: '哔哩哔哩', url: 'https://www.bilibili.com'},
+}
+const open = url => {
+    let cmd
+    if (process.platform === 'win32') {
+        cmd = 'start'
+        return child_process.exec(`${cmd} ${url}`)
+    } else if (process.platform === 'linux') {
+        cmd = 'xdg-open'
+    } else if (process.platform === 'darwin') {
+        cmd = 'open'
+    }
+    child_process.exec(`${cmd} "${url}"`)
+}
+
+const getData = async ({query, utils: {logger}}) => {
+    const openItem = bookmarks[query]
+    if (openItem) {
+        return [{
+            title: `打开: ${openItem.name}`,
+            subTitle: openItem.url,
+            action() {
+                open(openItem.url)
+            }
+        }]
+    } else {
+        const result = []
+        Object.values(bookmarks).forEach(item => {
+            logger.debug(item)
+            result.push({
+                title: `打开: ${item.name}`,
+                subTitle: `${item.url}`,
+                action() {
+                    open(item.url)
+                }
+            })
+        })
+
+        return result
+    }
+}
+
+module.exports = {
+    name: 'Bookmarks',
+    quick: 'bk',
+    async query(pluginContext) {
+        return getData(pluginContext)
+    }
+}
